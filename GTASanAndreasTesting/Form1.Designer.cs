5f9eb25c6f62b894352ab0d4d598753e43ee7e8d
Mike <mevans407.me@gmail.com>
2024-01-26 22:02:03 -0500

    Add project files.

---
+++ GTASanAndreasTesting/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace GTASanAndreasTesting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 276);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Teleport";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(316, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Retrieve Addresses";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(316, 136);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 219);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(161, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Continuously update position";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 242);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "y:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 242);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(82, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "z:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(240, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(82, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 294);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(328, 164);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Save/Load Coordinates";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Save Position";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Location Name:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 24);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(225, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Load Position";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(316, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Select a location....";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Saved Locations List";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(9, 79);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Refresh Saved List";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(167, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Teleport To Selection";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 472);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "GTA San Andreas Testing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GTASanAndreasTesting/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json; // JSON.NET
+using XDevkit; // XBOX 360 SDK
+using JRPC_Client; // JRPC2
+
+namespace GTASanAndreasTesting
+{
+    public partial class Form1 : Form
+    {
+        bool dbgConnection = false;
+        public uint TP_ADDRESS = 0;
+
+        public IXboxManager xbm;
+        public XboxConsole xbdbg;
+
+        private bool DbgConnect()
+        {
+            try
+            {
+                xbm = new XboxManager();
+                xbdbg = xbm.OpenConsole(xbm.DefaultConsole);
+                xbdbg.OpenConnection(null);
+                dbgConnection = true;
+                return true;
+            }
+            catch
+            {
+                dbgConnection = false;
+                return false;
+            }
+        }
+
+        private bool EntryExists(List<Dictionary<string, object>> existingLocations, string locationName)
+        {
+            // Check if an entry with the same locationName already exists in the list
+            return existingLocations.Any(entry => entry.ContainsKey("locationName") && entry["locationName"].ToString() == locationName);
+        }
+
+        private void SaveCoords(string locationName, float x, float y, float z)
+        {
+            // Create a new dictionary to store coordinates and location name
+            Dictionary<string, object> coords = new Dictionary<string, object>();
+
+            // Add the location name to the dictionary
+            coords.Add("locationName", locationName);
+
+            // Add the coordinates to the dictionary
+            coords.Add("x", x);
+            coords.Add("y", y);
+            coords.Add("z", z);
+
+            // Define the common file name for all entries
+            string fileName = "all_locations.json";
+
+            // Check if the file already exists
+            if (System.IO.File.Exists(fileName))
+            {
+                // Read the existing content of the file
+                string existingJson = System.IO.File.ReadAllText(fileName);
+
+                // Deserialize the existing content into a list of dictionaries
+                List<Dictionary<string, object>> existingLocations = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(existingJson);
+
+                // Check if an entry with the same locationName already exists
+                if (!EntryExists(existingLocations, locationName))
+                {
+                    // Add the new coordinates to the list
+                    existingLocations.Add(coords);
+
+                    // Convert the updated list to a JSON string
+                    string updatedJson = JsonConvert.SerializeObject(existingLocations, Formatting.Indented);
+
+                    // Overwrite the file with the updated content
+                    System.IO.File.WriteAllText(fileName, updatedJson);
+                }
+            }
+            else
+            {
+                // Create a new list for the first entry
+                List<Dictionary<string, object>> locations = new List<Dictionary<string, object>>();
+                locations.Add(coords);
+
+                // Convert the list to a JSON string
+                string json = JsonConvert.SerializeObject(locations, Formatting.Indented);
+
+                // Write the JSON string to a new file
+                System.IO.File.WriteAllText(fileName, json);
+            }
+        }
+
+        private void LoadCoordinates(string locationName)
+        {
+            // Define the common file name for all entries
+            string fileName = "all_locations.json";
+
+            // Check if the file exists
+            if (System.IO.File.Exists(fileName))
+            {
+                // Read the existing content of the file
+                string existingJson = System.IO.File.ReadAllText(fileName);
+
+                // Deserialize the existing content into a list of dictionaries
+                List<Dictionary<string, object>> existingLocations = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(existingJson);
+
+                // Find the entry with the specified locationName
+                Dictionary<string, object> selectedEntry = existingLocations.FirstOrDefault(entry =>
+                    entry.ContainsKey("locationName") && entry["locationName"].ToString() == locationName);
+
+                if (selectedEntry != null)
+                {
+                    // Display coordinates in the respective TextBoxes
+                    textBox1.Text = selectedEntry.ContainsKey("x") ? selectedEntry["x"].ToString() : "";
+                    textBox2.Text = selectedEntry.ContainsKey("y") ? selectedEntry["y"].ToString() : "";
+                    textBox3.Text = selectedEntry.ContainsKey("z") ? selectedEntry["z"].ToString() : "";
+                }
+                else
+                {
+                    MessageBox.Show("Location not found in the file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File not found.");
+            }
+        }
+
+        private void LoadLocationNames()
+        {
+            // Define the common file name for all entries
+            string fileName = "all_locations.json";
+
+            // Check if the file exists
+            if (System.IO.File.Exists(fileName))
+            {
+                // Read the existing content of the file
+                string existingJson = System.IO.File.ReadAllText(fileName);
+
+                // Deserialize the existing content into a list of dictionaries
+                List<Dictionary<string, object>> existingLocations = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(existingJson);
+
+                // Retrieve all 'locationName' values
+                List<string> locationNames = existingLocations
+                    .Where(entry => entry.ContainsKey("locationName"))
+                    .Select(entry => entry["locationName"].ToString())
+                    .ToList();
+
+                // Populate the ComboBox with location names
+                comboBox1.DataSource = locationNames;
+            }
+            else
+            {
+                MessageBox.Show("File not found.");
+            }
+        }
+
+        private void WriteLine(string text)
+        {
+            richTextBox1.AppendText(text + "\n");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DbgConnect())
+                {
+                    MessageBox.Show("Connected to Debug Monitor!");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to connect to Debug Monitor!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dbgConnection)
+                {
+                    uint addr = 0x823A54AC;
+                    xbdbg.DebugTarget.ConnectAsDebugger("xdk", XboxDebugConnectFlags.Force);
+                    xbdbg.DebugTarget.SetBreakpoint(addr);
+                    try
+                    {
+                        xbdbg.OnStdNotify += (EventType, EventInfo) =>
+                        {
+                            if (EventType == XboxDebugEventType.ExecutionBreak && EventInfo.Info.Address == addr)
+                            {
+                                EventInfo.Info.Thread.TopOfStack.GetRegister64(XboxRegisters64.r26, out long r26);
+                                EventInfo.Info.Thread.TopOfStack.GetRegister64(XboxRegisters64.r30, out long r30);
+                                EventInfo.Info.Thread.TopOfStack.GetRegister64(XboxRegisters64.r11, out long r11);
+
+                                if (r26 == 0x2 && r30 == 0x1)
+                                {
+                                    Invoke(new Action(() =>
+                                    {
+                                        WriteLine("Current Ptr is: 0x" + r11.ToString("X"));
+                                        xbdbg.DebugTarget.RemoveAllBreakpoints();
+                                        WriteLine("Current DMA Address is: 0x" + (r11 + 0x30).ToString("X"));
+                                        TP_ADDRESS = (uint)(r11 + 0x30);
+                                        float pos_x = xbdbg.ReadFloat(TP_ADDRESS);
+                                        float pos_y = xbdbg.ReadFloat(TP_ADDRESS + 0x4);
+                                        float pos_z = xbdbg.ReadFloat(TP_ADDRESS + 0x8);
+                                        WriteLine("Current Position:");
+                                        WriteLine("X: " + pos_x.ToString());
+                                        WriteLine("Y: " + pos_y.ToString());
+                                        WriteLine("Z: " + pos_z.ToString());
+                                        textBox1.Text = pos_x.ToString();
+                                        textBox2.Text = pos_y.ToString();
+                                        textBox3.Text = pos_z.ToString();
+                                    }));
+                                }
+                                else
+                                {
+                                    Invoke(new Action(() =>
+                                    {
+                                        richTextBox1.AppendText("Serching...\n");
+                                    }));
+                                }
+
+                                EventInfo.Info.Thread.Continue(true);
+                                xbdbg.DebugTarget.Go(out bool flag2);
+                                xbdbg.DebugTarget.FreeEventInfo(EventInfo.Info);
+                            }
+                        };
+                    }
+                    catch { }
+                }
+                else
+                {
+                    MessageBox.Show("Debug Monitor not connected!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dbgConnection && TP_ADDRESS != 0 && checkBox1.Checked)
+                {
+                    float pos_x = xbdbg.ReadFloat(TP_ADDRESS);
+                    float pos_y = xbdbg.ReadFloat(TP_ADDRESS + 0x4);
+                    float pos_z = xbdbg.ReadFloat(TP_ADDRESS + 0x8);
+                    textBox1.Text = pos_x.ToString();
+                    textBox2.Text = pos_y.ToString();
+                    textBox3.Text = pos_z.ToString();
+                }
+            }
+            catch
+            {
+                timer1.Stop();
+                MessageBox.Show("Failed to read position!");
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                xbdbg.DebugTarget.RemoveAllBreakpoints();
+                xbdbg.DebugTarget.DisconnectAsDebugger();
+            }
+            catch { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveCoords(textBox4.Text, float.Parse(textBox1.Text), float.Parse(textBox2.Text), float.Parse(textBox3.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string locationNameToLoad = textBox4.Text;
+            LoadCoordinates(locationNameToLoad);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadLocationNames();
+        }
+    }
+}
